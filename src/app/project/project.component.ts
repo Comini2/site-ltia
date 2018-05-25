5a69a5b49d04e984e0f0ae3fb9795c3c43b8244f
Comini2 <joaocomini@gmail.com>
2018-05-25 20:04:02 -0300

    Pagina de projetos quase terminada.

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 import { JsonService } from '../json.service';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.css'],
-  providers: [JsonService]
+  providers: [JsonService],
+  animations: [
+    trigger('showProject', [
+      state('true', style({
+        transform: 'translateX(-50%)'
+    })),
+      state('false', style({
+        transform: 'translateX(0)'
+    })),
+      transition('* => *', animate('150ms ease-in-out'))
+  ])]
 })
 export class ProjectComponent implements OnInit {
 
   projects = [];
+  selectedProject : any;
+  show : boolean;
 
   constructor(private jsonService : JsonService) {
     jsonService.getJSON('assets/json/projects.json', (data) => {
@@ -19,5 +32,18 @@ export class ProjectComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  selectProject(project : any){
+    this.show = true;
+    this.selectedProject = project;
+  }
+
+  generateArray(obj){
+    return Object.keys(obj).map((key)=>{ return obj[key]});
+ }
+
+ moveTop() {
+   window.scroll(window.scrollX, 0);
+ }
 
 }
