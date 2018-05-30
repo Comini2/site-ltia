286dd0118419bd52562d3e746eb16676540262f0
João Comini <joao.comini@fc.unesp.br>
2018-05-30 03:51:09 -0300

    Correção de bugs e adição do componente de imagem geral

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, Renderer2, OnDestroy, ViewChildren, QueryList } from '@angular/core';
-import { Location } from '@angular/common';
+import { Component, OnInit, AfterViewInit} from '@angular/core';
 import { JsonService } from '../json.service';
 import { trigger, transition, query, style, animate, stagger } from '@angular/animations';
+import { ActivatedRoute, Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-about',
@@ -18,54 +18,51 @@ import { trigger, transition, query, style, animate, stagger } from '@angular/an
 })
 export class AboutComponent implements AfterViewInit, OnInit {
 
-  @ViewChild("scrollTarget") target : ElementRef;
-  @ViewChildren("img") imgs : QueryList<ElementRef>;
   participations : any;
   selectedParticipation : any;
   activeButton : number = -1;
-  loadedImgs : number = 0;
+  private fragment : string;
 
-  constructor(private jsonService : JsonService, private location : Location, private renderer : Renderer2) { }
+  constructor(private jsonService : JsonService, private route: ActivatedRoute, private router : Router) { }
 
   ngOnInit() {
+    this.route.fragment.subscribe(fragment => {this.fragment = fragment});
+
+    this.router.events.subscribe( e => {
+      if( e instanceof NavigationEnd) {
+        const tree = this.router.parseUrl(this.router.url);
+        try{
+          document.querySelector('#' + this.fragment).scrollIntoView();
+          window.scrollBy(0, -80);
+        } catch (e) {}
+      }
+    });
+
     this.jsonService.getJSON('../assets/json/teams.json', (data) => {
       this.participations = data;
     })
   }
 
   ngAfterViewInit(){
-
-    if(this.location.path() === '/ltia-no-mundo'){
-      if(this.loadedImgs === this.imgs.length){
-        this.smoothScroll(250);
-        return;
-      }
-      this.imgs.forEach(img => {
-        this.renderer.listen(img.nativeElement, 'load', () => {
-          this.loadedImgs++;
-          if(this.loadedImgs === this.imgs.length)
-            this.smoothScroll(250);
-        })
-      });
-    }
-    else{
-      window.scrollTo(0, 0);
-    }  
+    try{
+      document.querySelector('#' + this.fragment).scrollIntoView();
+      window.scrollBy(0, -80);
+    } catch (e) {}
   }
 
-  smoothScroll(totalTime : number){
-    let deltaTime = 1000/60;
-    let targetPosition = this.target.nativeElement.offsetTop - this.target.nativeElement.offsetHeight;
-    let speed = targetPosition/totalTime;
-    let loops = targetPosition/(speed*deltaTime);
-    let currentPosition = 0;
-    for(let i = 0; i<loops; i++){
-      setTimeout(() => {
-        currentPosition += speed*deltaTime;
-        window.scrollTo(0, currentPosition);
-      }, deltaTime*i);
-    }
-  }
+  // smoothScroll(totalTime : number){
+  //   let deltaTime = 1000/60;
+  //   let targetPosition = this.target.nativeElement.offsetTop - this.target.nativeElement.offsetHeight;
+  //   let speed = targetPosition/totalTime;
+  //   let loops = targetPosition/(speed*deltaTime);
+  //   let currentPosition = 0;
+  //   for(let i = 0; i<loops; i++){
+  //     setTimeout(() => {
+  //       currentPosition += speed*deltaTime;
+  //       window.scrollTo(0, currentPosition);
+  //     }, deltaTime*i);
+  //   }
+  // }
 
   selectYear(index){
     this.activeButton = index;
